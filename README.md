2ff231657b1fb30847c9d1b992788b1fa8b443f0
Anand Uthaman <39004869+AdroitAnandAI@users.noreply.github.com>
2020-05-29 18:51:13 +0530

    DNN Custom Models
    DNN Custom Model code

--- README.md
+++ README.md
@@ -1,1 +1,106 @@
 # Indian Accent Speech Recognition
+Traditional ASR (Signal & Cepstral Analysis, DTW, HMM, Probabilistic Modelling) & DNNs (Custom Models + DeepSpeech) on Indian Accent Speech
+
+To understand the context, theory and explanation of this project, head over to my blog:
+https://medium.com/@AnandAI/self-driving-car-on-indian-roads-4e305cb04198
+
+
+# How to Use?
+
+## Contents:
+
+-----
+DeepSpeech_Training.ipynb: Retraining of DeepSpeech Model with Indian Accent Voice Data.
+
+## Data Source/ Training Data:
+
+Indic TTS Project: Downloaded 50+ GB of Indic TTS voice DB from Speech and Music Technology Lab,  IIT Madras, which comprises of 10000+ spoken sentences from 20+ states (both Male and Female native speakers)
+
+https://www.iitm.ac.in/donlab/tts/index.php
+
+You can also record your own audio or let the ebook reader apps read a document. But I found it is insufficient to train such a heavy model. Then I requested support of IIT Madras, Speech Lab who kindly granted access to their Voice database.
+
+## DNN Custom Models:
+
+### Model 1: CNN + RNN + TimeDistributed Dense
+
+![](images/Model1.jpg)
+
+### Model 2: Deeper RNN + TimeDistributed Dense
+
+![](images/Model2.jpg)
+
+### Comparison Plot
+
+### Model 3: Pooled CNN+Deep Bidirectional RNN +Time-distributed Dense
+
+![](images/Model3.jpg)
+
+
+## Steps to Follow:
+- Getting a pre-trained model.
+- Steps for loading the dataset of Indian Accent English Speech.
+- Steps to transfer learning with the released model.
+- Comparing results between trained model and the Original DeepSpeech model.
+
+
+
+## Step by step instructions
+
+- 
+- The dataset contains the audio and its description. But to load the data to deep speech model, we need to generate CSV containing audio file path, its transcription and file size.
+- Split the CSV file into 3 parts: test.csv,train.csv and valid.csv.
+- Write a python program that automatically set the frame rate for all audio files into 12000hz (deep speech model requirement)
+- Clone the Baidu DeepSpeech Project 0.5.1 from here
+- Execute DeepSpeech.py with appropriate parameters.
+- Export_dir will contain output_graph.pbmm which you load in deepspeech.model() function.
+- KenLM ToolKit is used to generate Trie file. It is required to pass in to deep speech decoder function.
+- model.enableDecoderWithLM(lm_file,trie,0.75,1.85): lm_file is the .pbmm after training and trie is the output of KenLM Toolkit.
+- Use deep speech decoder function to do STT.
+
+```
+./DeepSpeech.py --train_files ../data/CV/en/clips/train.csv --dev_files ../data/CV/en/clips/dev.csv --test_files ../data/CV/en/clips/test.csv
+```
+
+To fine-tune the entire graph using data in train.csv, dev.csv, and test.csv, for 3 epochs we can tune hyperparameters as below,
+```
+python3 DeepSpeech.py --n_hidden 2048 --checkpoint_dir path/to/checkpoint/folder --epochs 3 --train_files my-train.csv --dev_files my-dev.csv --test_files my_dev.csv --learning_rate 0.0001
+```
+
+Hyperparameters for Training:
+
+```
+python -u DeepSpeech.py \
+   --train_files /home/prem/ds_project/datavoice/data_voice_train.csv \
+   --test_files /home/prem/ds_project/datavoice/data_voice_test.csv \
+   --dev_files /home/prem/ds_project/datavoice/data_voice_dev.csv \
+   --n_hidden 2048 \
+   --epoch 100 \
+   --use_seq_length False \
+   --checkpoint_dir /home/prem/ds_project/datavoice/checkpoints/ \
+   --learning_rate 0.0001 \
+   --export_dir /home/prem/ds_project/datavoice/model_export/ \
+   --train_batch_size 64 \
+   --test_batch_size 32 \
+   --dev_batch_size 32 \
+```
+
+
+## Comparing Indian Accent English Model with Deepspeech model
+To check accuracy, we used 3 metrics: WER, WACC and BLUE SCORE.
+
+![](images/metrics.jpg)
+
+Metric shows trained model performs much better for Indian Accent English.
+
+## Model Comparison Results:
+
+Lets plot above metrics, feeding Indian Accent Speech Data (Test Set) to both DeepSpeech pre-trained model and our trained model to compare. The 3 bins in graphs below represents low, medium and high accuracy, from left to right.
+
+### DeepSpeech Base Model: Most datapoints classified as "Low Accuracy" in all 3 metrics
+![](images/graph1.jpg)
+
+### Trained Model: Most datapoints classified as "Medium & High Accuracy" in all 3 metrics
+![](images/graph2.jpg)
+
+The above depiction proves that the trained model performs much better for Indian Accent Speech Recognition compared to DeepSpeech model.
---
+++ sample_models.py
@@ -0,0 +1,171 @@
+from keras import backend as K
+from keras.models import Model
+from keras.layers import (BatchNormalization, Conv1D, Dense, Input, 
+    TimeDistributed, Activation, Bidirectional, SimpleRNN, GRU, LSTM, MaxPooling1D)
+
+def simple_rnn_model(input_dim, output_dim=29):
+    """ Build a recurrent network for speech 
+    """
+    # Main acoustic input
+    input_data = Input(name='the_input', shape=(None, input_dim))
+    # Add recurrent layer
+    simp_rnn = GRU(output_dim, return_sequences=True, 
+                 implementation=2, name='rnn')(input_data)
+    # Add softmax activation layer
+    y_pred = Activation('softmax', name='softmax')(simp_rnn)
+    # Specify the model
+    model = Model(inputs=input_data, outputs=y_pred)
+    model.output_length = lambda x: x
+    print(model.summary())
+    return model
+
+def rnn_model(input_dim, units, activation, output_dim=29):
+    """ Build a recurrent network for speech 
+    """
+    # Main acoustic input
+    input_data = Input(name='the_input', shape=(None, input_dim))
+    # Add recurrent layer
+    simp_rnn = GRU(units, activation=activation,
+        return_sequences=True, implementation=2, name='rnn')(input_data)
+    # TODO: Add batch normalization 
+    bn_rnn = BatchNormalization(name='simp_rnn')(simp_rnn)
+    # TODO: Add a TimeDistributed(Dense(output_dim)) layer
+    time_dense = TimeDistributed(Dense(output_dim))(bn_rnn)
+    # Add softmax activation layer
+    y_pred = Activation('softmax', name='softmax')(time_dense)
+    # Specify the model
+    model = Model(inputs=input_data, outputs=y_pred)
+    model.output_length = lambda x: x
+    print(model.summary())
+    return model
+
+
+def cnn_rnn_model(input_dim, filters, kernel_size, conv_stride,
+    conv_border_mode, units, output_dim=29):
+    """ Build a recurrent + convolutional network for speech 
+    """
+    # Main acoustic input
+    input_data = Input(name='the_input', shape=(None, input_dim))
+    # Add convolutional layer
+    conv_1d = Conv1D(filters, kernel_size, 
+                     strides=conv_stride, 
+                     padding=conv_border_mode,
+                     activation='relu',
+                     name='conv1d')(input_data)
+    # Add batch normalization
+    bn_cnn = BatchNormalization(name='bn_conv_1d')(conv_1d)
+    # Add a recurrent layer
+    simp_rnn = SimpleRNN(units, activation='relu',
+        return_sequences=True, implementation=2, name='rnn')(bn_cnn)
+    # TODO: Add batch normalization
+    bn_rnn = BatchNormalization(name='simp_rnn')(simp_rnn)
+    # TODO: Add a TimeDistributed(Dense(output_dim)) layer
+    time_dense = TimeDistributed(Dense(output_dim))(bn_rnn)
+    # Add softmax activation layer
+    y_pred = Activation('softmax', name='softmax')(time_dense)
+    # Specify the model
+    model = Model(inputs=input_data, outputs=y_pred)
+    model.output_length = lambda x: cnn_output_length(
+        x, kernel_size, conv_border_mode, conv_stride)
+    print(model.summary())
+    return model
+
+def cnn_output_length(input_length, filter_size, border_mode, stride,
+                       dilation=1):
+    """ Compute the length of the output sequence after 1D convolution along
+        time. Note that this function is in line with the function used in
+        Convolution1D class from Keras.
+    Params:
+        input_length (int): Length of the input sequence.
+        filter_size (int): Width of the convolution kernel.
+        border_mode (str): Only support `same` or `valid`.
+        stride (int): Stride size used in 1D convolution.
+        dilation (int)
+    """
+    if input_length is None:
+        return None
+    assert border_mode in {'same', 'valid'}
+    dilated_filter_size = filter_size + (filter_size - 1) * (dilation - 1)
+    if border_mode == 'same':
+        output_length = input_length
+    elif border_mode == 'valid':
+        output_length = input_length - dilated_filter_size + 1
+    return (output_length + stride - 1) // stride
+
+def deep_rnn_model(input_dim, units, recur_layers, output_dim=29):
+    """ Build a deep recurrent network for speech 
+    """
+    # Main acoustic input
+    input_data = Input(name='the_input', shape=(None, input_dim))
+    # TODO: Add recurrent layers, each with batch normalization
+    prev_layer = input_data
+    for layer in range(recur_layers):
+        prev_layer = GRU(units, activation='relu',
+            return_sequences=True, implementation=2, name=f'rnn_{layer+1}')(prev_layer)
+        # TODO: Add batch normalization
+        prev_layer = BatchNormalization()(prev_layer)
+    
+    # TODO: Add a TimeDistributed(Dense(output_dim)) layer
+    time_dense = TimeDistributed(Dense(output_dim))(prev_layer)
+    # Add softmax activation layer
+    y_pred = Activation('softmax', name='softmax')(time_dense)
+    # Specify the model
+    model = Model(inputs=input_data, outputs=y_pred)
+    model.output_length = lambda x: x
+    print(model.summary())
+    return model
+
+def bidirectional_rnn_model(input_dim, units, output_dim=29):
+    """ Build a bidirectional recurrent network for speech
+    """
+    # Main acoustic input
+    input_data = Input(name='the_input', shape=(None, input_dim))
+    # TODO: Add bidirectional recurrent layer
+    bidir_rnn = Bidirectional(GRU(units, activation='relu',
+            return_sequences=True, implementation=2, name='bi-rnn'))(input_data)
+    # TODO: Add a TimeDistributed(Dense(output_dim)) layer
+    time_dense = TimeDistributed(Dense(output_dim))(bidir_rnn)
+    # Add softmax activation layer
+    y_pred = Activation('softmax', name='softmax')(time_dense)
+    # Specify the model
+    model = Model(inputs=input_data, outputs=y_pred)
+    model.output_length = lambda x: x
+    print(model.summary())
+    return model
+
+def final_model(input_dim, filters, kernel_size, conv_stride, 
+                conv_border_mode, units, recur_layers, output_dim=29):
+    """ Build a deep network for speech 
+    """
+    # Main acoustic input
+    input_data = Input(name='the_input', shape=(None, input_dim))
+    # TODO: Specify the layers in your network
+    # Add convolutional layer
+    conv_1d = Conv1D(filters, kernel_size, 
+                     strides=conv_stride, 
+                     padding=conv_border_mode,
+                     activation='relu',
+                     name='conv1d')(input_data)
+    pooled_1d = MaxPooling1D(pool_size=2, strides=1)(conv_1d)
+    # Add batch normalization
+    bn_cnn = BatchNormalization(name='bn_conv_1d')(pooled_1d)
+    
+    prev_layer = bn_cnn
+    for layer in range(recur_layers):
+        prev_layer = Bidirectional(GRU(units, activation='relu',
+            return_sequences=True, implementation=2, name=f'gru_{layer+1}'))(prev_layer)
+        # TODO: Add batch normalization
+        prev_layer = BatchNormalization()(prev_layer)
+    
+    # TODO: Add a TimeDistributed(Dense(output_dim)) layer
+    time_dense = TimeDistributed(Dense(output_dim))(prev_layer)
+    
+    # TODO: Add softmax activation layer
+    y_pred = Activation('softmax', name='softmax')(time_dense)
+    # Specify the model
+    model = Model(inputs=input_data, outputs=y_pred)
+    # TODO: Specify model.output_length
+    model.output_length = lambda x: cnn_output_length(
+        x, kernel_size, conv_border_mode, conv_stride)/2
+    print(model.summary())
+    return model
